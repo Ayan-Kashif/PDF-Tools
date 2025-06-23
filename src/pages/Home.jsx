e68021006ed3f7b5fe5253413dd342333545dfbc
Ayan-Kashif <mrayan.buiz@gmail.com>
2025-06-23 13:52:11 +0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,6 +12,10 @@ const Home = () => {
     if (stored && stored !== theme) setTheme(stored);
   }, []);
 
+    useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   const isDark = theme === 'dark';
 
   const bgMain = isDark ? 'bg-gray-900 text-white' : 'bg-gray-100 text-gray-900';
