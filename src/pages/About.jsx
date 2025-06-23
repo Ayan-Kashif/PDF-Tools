a68a23d572ac24f012fce81ff9544f3d79a7ed2f
Ayan-Kashif <mrayan.buiz@gmail.com>
2025-06-24 00:55:21 +0500

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React,{ useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import { FaLightbulb, FaCode, FaUsers } from 'react-icons/fa';
 
