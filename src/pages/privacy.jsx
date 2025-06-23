72c657e7757a4c47d11e4a20ce9ffedccfdddba1
Ayan-Kashif <mrayan.buiz@gmail.com>
2025-06-23 13:53:31 +0500

    Update privacy.jsx

--- src/pages/privacy.jsx
+++ src/pages/privacy.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import { Helmet } from 'react-helmet';
 import { Link } from 'react-router-dom';
 import { FaShieldAlt, FaLock, FaUserCog, FaDatabase, FaCookie } from 'react-icons/fa';
 
 const PrivacyPolicy = () => {
+    useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="privacy-policy-page">
       <Helmet>
